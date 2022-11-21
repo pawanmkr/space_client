73fac085f1466f662db11b29cb21e0ba9e9da632
pawanmkr <iampawanmkr@gmail.com>
2022-11-21 08:57:31 +0530

    first commit

---
+++ utils/formatMessage.js
@@ -0,0 +1,11 @@
+let moment = require('moment');
+
+const formatMessage = (username, text) => {
+    return {
+        username,
+        text,
+        time: moment().format('h:mm a')
+    }
+}
+
+module.exports = formatMessage;
