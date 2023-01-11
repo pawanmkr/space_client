6f6bb0c82ed9a68a97c9892046f1f1ca5b7d668e
Pawan Kumar <zxcvbnm@192.168.1.2>
2023-01-11 13:06:22 +0530

    homepage form redesigned and rocket removed

--- src/pages/Home/Left.jsx
+++ src/pages/Home/Left.jsx
@@ -3,22 +3,27 @@ const Left = ({ space, handleFetch, setSpace }) => {
   return (
     <div className="left d-flex justify-content-center align-items-center">
         <div className="form">
-            <h3 className="form-heading">Create Chat Room</h3>
+            <h3 className="form-heading">You Ready?</h3>
             <form>
+                <div className="form-item">
+                    <input type="text" className="form-input" placeholder="Space name" value={space} onChange={(e) => setSpace(e.target.value)}/>
+                </div>
                 <div className="form-item">
                     <input type="text" className="form-input" placeholder="Username"/>
                 </div>
-                <div className="form-item">
-                    <input type="text" className="form-input" placeholder="room name" value={space} onChange={(e) => setSpace(e.target.value)}/>
-                </div>
-                <div className="form-item">
+                {/* <div className="form-item">
                     <input type="password" className="form-input" placeholder="password"/>
-                </div>
+                </div> */}
                 <div className="form-item">
-                    <button type="submit" className="form-submit-btn" onClick={handleFetch}>Create Room</button>
+                    <button type="submit" className="form-submit-btn" onClick={handleFetch}>Create New</button>
+                </div>
+                <h4 className="form-item or-label">
+                    or
+                </h4>
+                <div className="form-item">
+                    <button type="submit" className="form-join-btn">Join</button>
                 </div>
             </form>
-            <p className="join">Already have one ? <span className="join-span"><a href="#">Join</a></span></p>
         </div>
     </div>
   )
